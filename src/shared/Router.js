6a0478e2f385cb6f3e528adc0589d5902309e6bf
lee123so <100946922+lee123so@users.noreply.github.com>
2022-09-12 19:51:55 +0900

    .

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -3,14 +3,17 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Main from "../pages/Main";
 import Login from "../pages/Login";
 import SignUp from "../pages/SignUp";
+import Search from "../pages/Search";
+import DetailSearch from "../pages/DetailSearch";
+import Passenger from "../pages/Passenger";
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
           <Route path ="/" element = {<Main/>}/>
-          {/* <Route path ="/post" element = {<AddForm/>}/>
-          <Route path ="/detail/:id" element = {<Detail/>}/>
-          <Route path ="/addform" element = {<AddForm/>}/> */}
+          <Route path ="/search" element = {<Search/>}/>
+          <Route path ="/detail" element = {<DetailSearch/>}/>
+          <Route path ="/passenger" element = {<Passenger/>}/>
           <Route path ="/login" element = {<Login/>}/>
           <Route path ="/signup" element = {<SignUp/>}/>
         </Routes>
